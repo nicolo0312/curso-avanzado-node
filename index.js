f340b802311c60c4d02de20d88f900e6e506bc12
moranni <moranni@globalhitss.com>
2021-04-13 14:40:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const EventEmmiter = require('events')
+
+class PLatziVerseAgent extends EventEmmiter{
+    constructor(){
+        super()
+    }
+
+}
+
+module.exports = {PLatziVerseAgent}
